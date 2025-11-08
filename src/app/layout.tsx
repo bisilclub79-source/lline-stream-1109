89b0bfc86fb773d87a0146cf00aaa4a0c392b302
Peter Lee <bisilclub79@gmail.com>
2025-11-08 15:57:08 +0000

    안녕하세요. 지금부터 포트원(PortOne) V2 결제 시스템을 이용한 웹 앱의 전체 결제 기능을 구축 시작하겠습니다. 사용자는

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <script src="https://cdn.portone.io/v2/browser-sdk.js"></script>
       </head>
       <body
         className={cn(
